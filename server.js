1353061efae2383c4745fda1a894d8816d7118c5
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2022-02-25 05:02:51 +0200

    new repo

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const todosRouter = require("./routes/apis");
+
+const PORT = process.env.PORT || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Todo List API",
+      version: "1.0.0",
+      description: "A TodoList API",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/apis.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+const app = express();
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/tasks", todosRouter);
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
